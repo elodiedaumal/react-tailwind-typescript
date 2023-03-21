5fa8ca300cab15ad60bc76c0d527b9f150fc1a54
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-03-21 13:58:24 +0100

    done with hero section
    started benefit section

--- src/pages/Benefits.tsx
+++ src/pages/Benefits.tsx
@@ -1,10 +1,40 @@
-import { SelectedPage } from '../shared/types';
+import { SelectedPage, BenefitType } from '../shared/types';
 import {
   HomeModernIcon,
   UserGroupIcon,
   AcademicCapIcon,
 } from '@heroicons/react/24/solid';
 import { motion } from 'framer-motion';
+import Htext from '../shared/Htext';
+import Benefit from '../Components/Benefit';
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className='h-6 w-6' />,
+    title: 'State of the Art Facilities',
+    description:
+      'Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.',
+  },
+  {
+    icon: <UserGroupIcon className='h-6 w-6' />,
+    title: "100's of Diverse Classes",
+    description:
+      'Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.',
+  },
+  {
+    icon: <AcademicCapIcon className='h-6 w-6' />,
+    title: 'Expert and Pro Trainers',
+    description:
+      'Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.',
+  },
+];
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -16,11 +46,31 @@ const Benefits = ({ setSelectedPage }: Props) => {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-        <div>
-          <h1 className='basis-3/5 font-montserrat text-3xl font-bold'>
-            Benefits
-          </h1>
+        <div className='md:my-5 md:w-3/5'>
+          <Htext>More than just gym</Htext>
+          <p className='my-5 text-sm'>
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
         </div>
+        <motion.div
+          initial='hidden'
+          whileInView='visible'
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+          className='md:flex items-center justify-between gap-8 mt-5'
+        >
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </motion.div>
       </motion.div>
     </section>
   );
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -4,3 +4,8 @@ export enum SelectedPage {
   OurClasses = 'ourClasses',
   ContactUs = 'contactUs',
 }
+export interface BenefitType {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
